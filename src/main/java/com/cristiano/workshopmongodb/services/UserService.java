d53ab08bacc2b1cde7883a1867ed87409f1ee719
Cristiano Tiago Modesto <cristianoufo@gmail.com>
2019-10-14 08:57:58 -0300

    Atualização de usuário com PUT

--- src/main/java/com/cristiano/workshopmongodb/services/UserService.java
+++ src/main/java/com/cristiano/workshopmongodb/services/UserService.java
@@ -35,6 +35,16 @@ public class UserService {
 		userRepository.deleteById(id);
 	}
 
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return userRepository.save(newObj);
+		}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getEmail(), objDTO.getNome());
 	}
