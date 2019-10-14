dcafe407df1d7da2e1f08587215ac38fa52f4f75
Cristiano Tiago Modesto <cristianoufo@gmail.com>
2019-10-14 10:24:56 -0300

    Consulta simples com @Query

--- src/main/java/com/cristiano/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/cristiano/workshopmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.cristiano.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cristiano.workshopmongodb.domain.Post;
@@ -11,6 +12,7 @@ import com.cristiano.workshopmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
